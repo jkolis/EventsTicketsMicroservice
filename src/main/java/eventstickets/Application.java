aeb08b191d68db200853f9a7bd11709e5440e51f
Joanna Kolis <joanna.kolis@gmail.com>
2018-05-19 15:07:02 +0200

    Fixed problems with headers

--- src/main/java/eventstickets/Application.java
+++ src/main/java/eventstickets/Application.java
@@ -12,12 +12,15 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import static springfox.documentation.builders.PathSelectors.regex;
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @SpringBootApplication
 @RestController
 @EnableSwagger2
-public class Application {
+public class Application implements Filter {
 
     @RequestMapping("/")
     public String hello() {
@@ -49,5 +52,32 @@ public class Application {
                 "API License URL", null
         );
         return apiInfo;
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response =  (HttpServletResponse) res;
+
+//        response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+        response.setHeader("Access-Control-Allow-Origin", "*");
+
+//        response.setHeader("Acess-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
+        response.setHeader("Access-Control-Max-Age", "3600");
+//        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, Authorization");
+
+        chain.doFilter(req, res);
+
+    }
+
+    @Override
+    public void destroy() {
+
     }
 }
